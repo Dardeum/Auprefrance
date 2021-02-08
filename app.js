cab1e4531b7526fcc72f690e25297a75a903fecb
Dardeum <78161167+Dardeum@users.noreply.github.com>
2021-02-08 16:24:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bonjour la POEI Inetum!'));
+app.get('/', (req, res) => res.send('Bonjour la POEI Inetum! Ceci est un test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
